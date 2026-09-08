e296f59daf0f5469f206b2337c8e4ed7faed48c0
Felipmayo <felipedemoraismayo@gmail.com>
2026-09-08 09:05:57 -0300

    Update exerc1.py

--- exerc1.py
+++ exerc1.py
@@ -150,3 +150,17 @@ elif chovendo == resposta == "not" or "não":
   print("Saia tranquilo")
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+print("DESCONTO PROGRESSIVO")
+compra = int(input("Insira o valor da compra: "))
+
+if compra >= 100 and compra < 300:
+  print("O valor da compra ficou",compra-(compra*0.10))
+elif compra >= 300 and compra < 500:
+  print("O valor da compra ficou",compra-(compra*0.15))
+elif compra >= 500:
+  print("O valor da compra ficou",compra-(compra*0.20))
+else:
+  print("Sua compra não tem desconto. O valor da compra",compra)
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
